38f1212be6d910ee1a536975505de337447d0e97
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-11-23 17:54:30 +0300

    Refactor docstrings, chapter 3 problems

--- chapter_2/fizzBuzz.js
+++ chapter_2/fizzBuzz.js
@@ -1,8 +1,8 @@
-/*
- Write a program that uses console.log to print all the numbers from 1 to 100,
- with two exceptions. For numbers divisible by 3, print "Fizz" instead of the
- number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
- */
+/**
+ * Write a program that uses console.log to print all the numbers from 1 to 100,
+ * with two exceptions. For numbers divisible by 3, print "Fizz" instead of the
+ * number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+ **/
 
 for (var i = 0; i <= 100; i++) {
     var output = '';
--- chapter_2/loopingTriangle.js
+++ chapter_2/loopingTriangle.js
@@ -1,14 +1,14 @@
-/*
- Write a loop that makes seven calls to console.log to output the following triangle:
-
- #
- ##
- ###
- ####
- #####
- ######
- #######
- */
+/**
+ * Write a loop that makes seven calls to console.log to output the following triangle:
+ *
+ * #
+ * ##
+ * ###
+ * ####
+ * #####
+ * ######
+ * #######
+ **/
 
 var output = '#';
 for (var i = 0; i < 7; i++) {
